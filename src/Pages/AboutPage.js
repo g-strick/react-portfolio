f801dfba1fe9346f93dc0f5a5e24eb5f399fc959
G Strick <graysonstricker@gmail.com>
2021-02-15 16:38:44 -0500

    add title component to about page

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Title from "../Components/Title";
 
 const ContactsPage = () => {
   return (
     <div>
-      <h1>About Page</h1>
+      <Title title={"About Me"} span={"About Me"} />
     </div>
   );
 };
