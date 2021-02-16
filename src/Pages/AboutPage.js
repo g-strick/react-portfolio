1451ab524208475462350748f210008ec53243a1
G Strick <graysonstricker@gmail.com>
2021-02-16 12:51:41 -0500

    update about page

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Title from "../Components/Title";
-
+import ImageSection from "../Components/ImageSection";
+import SkillsSection from "../Components/SkillsSection";
 const ContactsPage = () => {
   return (
     <div>
       <Title title={"About Me"} span={"About Me"} />
+      <ImageSection />
+      <Title title={"My Skills"} span={"My Skills"} />
+      <SkillsSection skill={"javascript"} progress={"70"} />
     </div>
   );
 };
