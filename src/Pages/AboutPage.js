7ab79f433323fa5365b49c6908dbd2954449ef82
G Strick <graysonstricker@gmail.com>
2021-02-16 15:30:42 -0500

    update skill component and skill component styling

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -2,15 +2,22 @@ import React from "react";
 import Title from "../Components/Title";
 import ImageSection from "../Components/ImageSection";
 import SkillsSection from "../Components/SkillsSection";
-const ContactsPage = () => {
+const AboutPage = () => {
   return (
-    <div>
+    <div className="AboutPage">
       <Title title={"About Me"} span={"About Me"} />
       <ImageSection />
-      <Title title={"My Skills"} span={"My Skills"} />
-      <SkillsSection skill={"javascript"} progress={"70"} />
+      <div className="skillsContainer">
+        <SkillsSection skill={"Javascript"} progress={80} width={"80%"} />
+        <SkillsSection skill={"React"} progress={70} width={"70%"} />
+        <SkillsSection skill={"Node JS"} progress={60} width={"60%"} />
+        <SkillsSection skill={"SQL"} progress={30} width={"30%"} />
+        <SkillsSection skill={"Javascript"} progress={70} width={"70%"} />
+        <SkillsSection skill={"Web Design"} progress={80} width={"80%"} />
+        <SkillsSection skill={"UI/UX Design"} progress={30} width={"30%"} />
+      </div>
     </div>
   );
 };
 
-export default ContactsPage;
+export default AboutPage;
