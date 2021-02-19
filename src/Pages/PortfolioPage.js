a328193ccba9a2e2f0b82ea7d34a1f6f635213b7
G Strick <graysonstricker@gmail.com>
2021-02-19 12:45:20 -0500

    update portfolio

--- src/Pages/PortfolioPage.js
+++ src/Pages/PortfolioPage.js
@@ -1,11 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import Title from "../Components/Title";
+import Categories from "../Components/Categories";
+import MenuItems from "../Components/MenuItems";
+import portfolios from "../Components/AllPortfolios";
 
-const PortfolioPage = () => {
+const allCategories = [
+  "All",
+  ...new Set(portfolios.map((item) => item.category))
+];
+
+function PortfoliosPage() {
+  const [categories, setCategories] = useState(allCategories);
+  console.log("setCategories:", setCategories);
+  const [menuItems, setMenuItems] = useState(portfolios);
+
+  const filter = (category) => {
+    if (category === "All") {
+      setMenuItems(portfolios);
+      return;
+    }
+    const filteredData = portfolios.filter((item) => {
+      return item.category === category;
+    });
+    setMenuItems(filteredData);
+  };
+
   return (
-    <div>
-      <h1>Portfolio Page</h1>
+    <div className="PortfolioPage">
+      <Title title={"Portfolios"} span={"portfolios"} />
+
+      <div className="portfolios-data">
+        <Categories filter={filter} categories={categories} />
+        <MenuItems menuItem={menuItems} />
+      </div>
     </div>
   );
-};
+}
 
-export default PortfolioPage;
+export default PortfoliosPage;
