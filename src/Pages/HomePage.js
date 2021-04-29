7049a946461946701baff43c307b7e8ca6c11ef4
G-Strick <graysonstricker@gmail.com>
2021-04-28 22:08:00 -0400

    update bio and link to resume

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,7 +2,7 @@ import React from "react";
 import {
   faGithub,
   faLinkedinIn,
-  faHackerrank
+  faHackerrank,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -14,7 +14,26 @@ function HomePage() {
           Hi, I am
           <span> Grayson Stricker</span>
         </h1>
-        <p className="h-sub-text">This is my react portfolio website.</p>
+        <p className="h-sub-text">
+          Fullstack web developer with a passion for people and getting things
+          done. Effectively build more intuitive user experiences on the web by
+          leveraging an extensive sales background, with an interest for
+          computers. Recently earned a certificate in full-stack web development
+          from UNC Chapel Hill. Seeking an opportunity that will utilize this
+          skill set, while expanding it, to incorporate new technologies, skills
+          and techniques.
+        </p>
+        <div className="icons">
+          <button className="btn">
+            <a
+              href="https://drive.google.com/file/d/1s6u5iKB1tYjrf-47x_djGZ-eLkf6u6jq/view?usp=sharing"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Download CV
+            </a>
+          </button>
+        </div>
         <div className="icons">
           <a
             href="https://github.com/g-strick"
